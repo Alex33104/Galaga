b397f0c9afaa3224eada9f2c2e1d84c6172abad4
buh62 <harveycr@hendrix.edu>
2023-04-25 14:46:15 -0500

    y

--- Galaga/src/game/GalagaController.java
+++ Galaga/src/game/GalagaController.java
@@ -2,8 +2,11 @@ package game;
 
 import javafx.fxml.FXML;
 
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.shape.Arc;
 import javafx.scene.shape.Circle;
 
 import javax.swing.text.html.ImageView;
@@ -12,13 +15,31 @@ import java.awt.*;
 
 public class GalagaController {
     @FXML
-    private AnchorPane ship;
+    private Arc ship;
     @FXML
     private AnchorPane pane;
+    final double ship_speed = 5.0;
 
     @FXML
     public void initialize() {
         User captain = new User(25, 25);
+        pane.setOnKeyPressed((KeyEvent k) -> {
+            if (k.getCode() == KeyCode.LEFT) {
+                ship.setTranslateX(ship.getTranslateX() - ship_speed);
+            }
+            if (k.getCode() == KeyCode.RIGHT) {
+                ship.setTranslateX(ship.getTranslateX() + ship_speed);
+            }
+            if (k.getCode() == KeyCode.UP) {
+                ship.setTranslateY(ship.getTranslateY() - ship_speed);
+            }
+            if (k.getCode() == KeyCode.DOWN) {
+                ship.setTranslateY(ship.getTranslateY() + ship_speed);
+            }
+        });
     }
-
+    @FXML
+    public void left() {
+        ship.setTranslateX(ship.getTranslateX() - ship_speed);
+    }
 }
