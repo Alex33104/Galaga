f578b4689f3c91032cd90892ecac49a2071d83e5
Alex33104 <pangelinanag@hendrix.edu>
2023-04-26 02:24:54 -0500

    wednesday night work

--- Galaga/src/game/GalagaController.java
+++ Galaga/src/game/GalagaController.java
@@ -3,6 +3,7 @@ package game;
 import javafx.fxml.FXML;
 
 import javafx.scene.input.KeyCode;
+import javafx.animation.AnimationTimer;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
@@ -12,6 +13,7 @@ import javafx.scene.shape.Rectangle;
 
 import javax.swing.text.html.ImageView;
 import java.awt.*;
+import java.util.Queue;
 
 
 public class GalagaController {
@@ -19,13 +21,25 @@ public class GalagaController {
     private AnchorPane ship;
     @FXML
     private AnchorPane pane;
-    final double ship_speed = 5.0;
+    private Queue<User> u;
+    private double ship_speed = 5.0;
+    //Hey Chris this is just my attempt at animation for the bullets
+    private Movement clock;
+    private class Movement extends AnimationTimer {
+        private long FRAMES_PER_SEC = 50L;
+        private long INTERVAL = 1000000000L / FRAMES_PER_SEC;
+
+        private long last = 0;
+
+        @Override
+        public void handle(long l) {
+
+        }
+    }
 
     @FXML
     public void initialize() {
-        User captain = new User(25, 25);
-        UserView view = new UserView(captain);
-        pane.setOnKeyPressed((KeyEvent k) -> {
+        /*pane.setOnKeyPressed((KeyEvent k) -> {
             if (k.getCode() == KeyCode.A) {
                 ship.setTranslateX(ship.getTranslateX() - ship_speed);
             }
@@ -33,9 +47,24 @@ public class GalagaController {
                 ship.setTranslateX(ship.getTranslateX() + ship_speed);
             }
             if (k.getCode() == KeyCode.Z) {
-                view.shoot();
+
+            }
+        });*/
+        // creating new user and using the ship anchor pane to do something with it
+        User user = new User(90,120);
+        UserView view = new UserView(user, ship);
+        view.update();
+    }
+    //trying to link up the user class to some on-screen stuff
+    @FXML
+    private void setUser() {
+        User user = new User(25, 25);
+        UserView view = new UserView(user, ship);
+        pane.setOnKeyPressed((KeyEvent k) -> {
+            if (k.getCode() == KeyCode.A) {
             }
         });
+
     }
 
 }
--- Galaga/src/game/UserView.java
+++ Galaga/src/game/UserView.java
@@ -1,24 +1,56 @@
 package game;
 
-import javafx.fxml.FXML;
 import javafx.scene.Parent;
-import javafx.scene.shape.Arc;
-import javafx.scene.shape.Circle;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-
-import javax.swing.text.html.ImageView;
-import java.awt.*;
 
 public class UserView extends Parent {
     private User user;
-    private double dx = 7.5;
+    private AnchorPane ship;
 
-    public UserView(User user) {
+//tries to connect the controller to the view class, but it's harder than it looks
+    public UserView(User user, AnchorPane ship) {
         this.user = user;
+        this.ship = ship;
+        AnchorPane finalShip = ship;
+        ship.setOnKeyPressed((KeyEvent k) -> {
+                    if (k.getCode() == KeyCode.A) {
+                        System.out.println("hello");
+                        finalShip.setTranslateX(finalShip.getTranslateX() - 1);
+                    }
+                    if (k.getCode() == KeyCode.D) {
+                        finalShip.setTranslateX(finalShip.getTranslateX() + 1);
+                    }
+                });
+
+        ship = new AnchorPane();
+        ship.setScaleX(user.getX());
+        ship.setScaleY(user.getY());
+        ship.setPrefHeight(user.getH());
+        System.out.println(user.getH()); //should be 90
+        ship.setPrefWidth(user.getW());
+        System.out.println(user.getW()); //should be 120
+
+
     }
+// supposed to create a new bullet and shoot it in a straight line
     public void shoot() {
-        javafx.scene.shape.Rectangle bullet = new Rectangle();
+        System.out.println("hello");
+        Rectangle bullet = new Rectangle();
         bullet.setY(user.getY());
-        bullet.setX(dx);
+        bullet.setX(user.getX());
+        bullet.setFill(Color.BLACK);
+        bullet.setStroke(Color.RED);
+        getChildren().add(bullet);
+    }
+// new update method to hopefully see if it'll work when it's time
+    public void update() {
+        ship.setTranslateX(user.getX());
+        System.out.println(user.getX()); // Should be 0
+        ship.setTranslateY(user.getY());
+        System.out.println(user.getY()); // Should be 0
     }
 }
