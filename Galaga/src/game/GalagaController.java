a71e822568149c5af0d098cf44a045bbd47051a5
Alex33104 <pangelinanag@hendrix.edu>
2023-05-02 19:38:49 -0500

    hello

--- Galaga/src/game/GalagaController.java
+++ Galaga/src/game/GalagaController.java
@@ -7,6 +7,7 @@ import javafx.animation.AnimationTimer;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Rectangle;
+import java.util.Random;
 
 import java.util.Queue;
 
@@ -20,7 +21,9 @@ public class GalagaController {
     private AnchorPane pane;
     @FXML
     private Rectangle bullet;
+    private boolean move = true;
     private double speed = 0;
+    private Random rand = new Random();
     private final double ship_speed = 5.0;
     //Hey Chris this is just my attempt at animation for the bullets
     private Movement clock;
@@ -37,12 +40,18 @@ public class GalagaController {
                 bullet.setTranslateY(speed);
                 last = now;
                 speed -= 5;
-                System.out.println(bullet.getTranslateY());
-                if (bullet.getTranslateY() <= - 800) {
+                System.out.println(bullet.getTranslateX());
+                if (difference() <= 10 && difference() >= -10) {
+                    if (bullet.getTranslateY() <= -500) {
+                        target.setTranslateX(rand.nextInt(250) - 250);
+                    }
+                }
+                if (bullet.getTranslateY() <= - 750) {
                     System.out.println(ship.getTranslateY());
-                    bullet.setTranslateY(ship.getTranslateY());
+                    bullet.setTranslateX(ship.getTranslateX());
                     speed = 0;
                     clock.stop();
+                    move = true;
                 }
             }
         }
@@ -51,10 +60,22 @@ public class GalagaController {
     public void initialize() {
         // creating new user and using the ship anchor pane to do something with it
         User user = new User(90,120);
+        target.setTranslateX(rand.nextInt(250) - 250);
+        target.setTranslateX(30);
+        System.out.println(target.getTranslateX());
         UserView view = new UserView(user, ship);
         pane.setOnKeyPressed((KeyEvent k) -> {
             if (k.getCode() == KeyCode.Z) {
                 clock.start();
+                move = false;
+            }
+            if (move) {
+                if (k.getCode() == KeyCode.A) {
+                    bullet.setTranslateX(bullet.getTranslateX() - 5);
+                }
+                if (k.getCode() == KeyCode.D) {
+                    bullet.setTranslateX(bullet.getTranslateX() + 5);
+                }
             }
             view.moveAndShoot(k);
 
@@ -68,6 +89,12 @@ public class GalagaController {
             if (k.getCode() == KeyCode.Z) {
                 clock.start();
             }
+            if (k.getCode() == KeyCode.A) {
+                bullet.setTranslateX(ship.getTranslateX());
+            }
         });
+    }
+    public double difference() {
+        return bullet.getTranslateX() - (target.getTranslateX() - 20);
     }
 }
